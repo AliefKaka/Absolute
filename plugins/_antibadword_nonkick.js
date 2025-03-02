ff10bc30266ae5bc5042e5d6b0984dbc2e11061c
AliefKaka <aliefkaka123@gmail.com>
2025-03-02 22:20:47 +0700

    Update _antibadword_nonkick.js

--- plugins/_antibadword_nonkick.js
+++ plugins/_antibadword_nonkick.js
@@ -1,4 +1,4 @@
-let badwordRegex = /anj|asw|kont|ToIol|gblk|T0lol|Bgsd|ajn|anjingk|bajingan|bangsat|kontol|memek|pepekq|meki|titit|peler|tetek|toket|ngewe|goblok|tolol|idiot|ngentotd|jembut|bego|dajjal|jancuk|pantek|pukimak|kimak|kampang|lonte|colimek|pelacur|henceut|nigga|fuck|dick|bitch|tits|bastard|asshole/i; // tambahin sendiri
+let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|sshole/i; // tambahin sendiri
 
 async function before(m, { isBotAdmin }) {
     if (m.isBaileys && m.fromMe) return;
